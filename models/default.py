3bb07798d74f3a2ec7c6ea404e8f0545560164d1
fabioalves291 <fabioalvesdeandrade29@gmail.com>
2023-04-14 21:47:08 -0300

    in progress def urlq4

--- models/default.py
+++ models/default.py
@@ -1,7 +1,8 @@
 
 url1a3      = 'https://dados.ifrn.edu.br/dataset/d5adda48-f65b-4ef8-9996-1ee2c445e7c0/resource/00efe66e-3615-4d87-8706-f68d52d801d7/download/dados_extraidos_recursos_alunos-da-instituicao.json'
 urlq4Def    = 'https://resultados.tse.jus.br/oficial/ele,2022/544/dados-simplificados/br/br-c0001-e000544-r.json'
-urlq4       = list('https://resultados.tse.jus.br/oficial/ele',2022,"/544/dados-simplificados/br/","-e000544-r.json")
+def urllq4(ano,codigo3digpasta,cargo,codigodearquivopasta):
+    urlq4 = rf'https://resultados.tse.jus.br/oficial/ele{}/{}/dados-simplificados/br/{}{}-e000{}-r.json")
 
     #documentação https://www.tse.jus.br/eleicoes/eleicoes-2022/interessados-na-divulgacao-de-resultados-2022
 CodigodeEleição= {
