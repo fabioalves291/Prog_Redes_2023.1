535b95a61bdccb0351a5caa8954072211e0f2b78
fabioalves291 <fabioalvesdeandrade29@gmail.com>
2023-04-14 21:34:28 -0300

    adicionado codigos de turnos

--- models/default.py
+++ models/default.py
@@ -1,13 +1,16 @@
 
 url1a3      = 'https://dados.ifrn.edu.br/dataset/d5adda48-f65b-4ef8-9996-1ee2c445e7c0/resource/00efe66e-3615-4d87-8706-f68d52d801d7/download/dados_extraidos_recursos_alunos-da-instituicao.json'
-urlq4Def    = 'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json'
-urlq4       = 'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/'
-urlq4Final  = "-e000544-r.json"
+urlq4Def    = 'https://resultados.tse.jus.br/oficial/ele,2022/544/dados-simplificados/br/br-c0001-e000544-r.json'
+urlq4       = list('https://resultados.tse.jus.br/oficial/ele',2022,"/544/dados-simplificados/br/","-e000544-r.json")
 
     #documentação https://www.tse.jus.br/eleicoes/eleicoes-2022/interessados-na-divulgacao-de-resultados-2022
+CodigodeEleição= {
+"PrimeiroTurnoFederal":"544",
+"PrimeiroTurnoEstadual":"546"
+    }
 listcodigosdecargo  =   {
-"CodigoPresidente" :"0001",
-"CodigoGovernador" :"0003",
+"CodigoPresidente" : "0001",
+"CodigoGovernador" : "0003",
 "CodigoSenador"    : "0005",
 "CodigoDepFedera"  : "0006",
 "CodigoDepEstadua" : "0007",
